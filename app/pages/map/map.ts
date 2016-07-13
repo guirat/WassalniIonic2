ff8f07dcdfe488ade9d92de2604ad73b43d6355a
imen jmal <imen jmal>
2016-07-13 11:54:06 +0200

    geolocation

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -12,19 +12,34 @@ export class MapPage {
   ionViewLoaded() {
   
     this.confData.getMap().then(mapData => {
+      var currentPosition:google.maps.LatLng;
      let mapEle = document.getElementById('map');
      let map = new google.maps.Map(mapEle, {
         zoom: 16
       });
+
+      let markerCurrentPostion = new google.maps.Marker({
+          position: map.getCenter(),
+          icon:'img/BusStationMarker.png',
+          map: map,
+          animation: google.maps.Animation.DROP,
+          title: "I'm here"
+        });
+      var infowindow = new google.maps.InfoWindow({
+      content: "I'm here"
+      });
+      infowindow.open(map,markerCurrentPostion);
       let options = {timeout: 10000, enableHighAccuracy: true};
       Geolocation.getCurrentPosition(options).then((position) => {
          let lat = position.coords.latitude;
          let lng = position.coords.longitude;
-          map.setCenter(new google.maps.LatLng(lat,lng));
+         currentPosition=new google.maps.LatLng(lat,lng);
+          map.setCenter(currentPosition);
+          markerCurrentPostion.setPosition(currentPosition);
         }, (err) => {
             console.log('errrrrrrreur');
         });
-
+    
       mapData.forEach(markerData => {
         let infoWindow = new google.maps.InfoWindow({
           content: `<h5>${markerData.name}</h5>`
